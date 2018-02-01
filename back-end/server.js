b6c83bbc288bfb8c92735ea3dba5e70abb319d3b
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-01-31 18:50:02 -0700

    connects to server, CORS issue

--- back-end/server.js
+++ back-end/server.js
@@ -22,7 +22,7 @@ const router = require('./config/routes');
   //Only needed if not on Heroku/prod
   if(!process.env.DYNO) {
     app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+      res.header("Access-Control-Allow-Origin", "http://localhost:8100");
       res.header("Access-Control-Allow-Credentials", "true");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
--- front-end/src/pages/home/home.ts
+++ front-end/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public http : HttpClient
+  ) {
 
+  }
+
+  serverCheck(){
+    this.http.get('http://localhost:3000/').toPromise()
+    .catch(err => 
+        alert('Error:' + err)
+      )
+    .then(function(){
+      console.log('hit the server');
+    })
+    
   }
 
 }
