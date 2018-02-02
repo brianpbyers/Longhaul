cf131cdf4fd91b5d5b627c08edb90b8b07a3ac47
anhonestmouse <anhonestmouse@gmail.com>
2018-02-01 18:16:30 -0700

    bus route created

--- back-end/staticFileConverter.js
+++ back-end/staticFileConverter.js
@@ -23,23 +23,12 @@ DaBa.sequelize.sync({force: true}).then(function(){
 		DB.Route.bulkCreate(routesArray, {validate: true})
 		.then((createdRoutes)=>{
 			console.log("CREATED ROUTES!", createdRoutes);
-		// routesArray.forEach((route)=>{
-		// //routeId: route_id, routeName: route_long_name, routeColor: route_color
-		// 	DB.Route.create({
-		// 		name: route.route_id,
-		// 		description: route.route_long_name,
-		// 		color: route.route_color
-		// 	})
-		// 	.then((routeN)=>{
-		// 		console.log('Saved Route:',routeN.name);
-		// 	});
-		// });
 			console.log('end');
 
 			csv()
 			.fromFile('./StaticTripData/stops.txt')
 			.on('json',(jsonObj)=>{
-				if(jsonObj.stop_id) stopsArray.push({id_num:jsonObj.stop_id, name:jsonObj.stop_name});
+				if(jsonObj.stop_id) stopsArray.push({number:jsonObj.stop_id, name:jsonObj.stop_name});
 				else invalidStops.push(jsonObj);
 			})
 			.on('done',(err)=>{
@@ -48,16 +37,6 @@ DaBa.sequelize.sync({force: true}).then(function(){
 				DB.Stop.bulkCreate(stopsArray, {validate: true})
 				.then((createdStops)=>{
 					console.log("CREATED STOPS!");
-				// stopsArray.forEach((stop)=>{
-				// 	DB.Stop.create({
-				// 		id_num: stop.stop_id,
-				// 		name: stop.stop_name
-				// 	})
-				// 	.then((stopN)=>{
-				// 		console.log('Saved Stop:',stopN.name);
-				// 	});
-				// //stopId: stop_id, stopName: stop_name
-				// });
 
 				console.log('Ended');
 
@@ -76,19 +55,8 @@ DaBa.sequelize.sync({force: true}).then(function(){
 						console.log("Created Trips!");
 						process.exit();
 					});
-					// tripsArray.forEach((trip)=>{
-					// 	DB.Trip.create({
-					// 		route: trip.route_id,
-					// 		service: trip.service_id,
-					// 		trip: trip.trip_id
-					// 	})
-					// 	.then((tripN)=>{
-					// 		console.log('Saved Trip:',tripN.trip);
-					// 	});
-					// });
 
 					console.log('Finished!');
-					// process.exit();
 					});
 				});
 			});
