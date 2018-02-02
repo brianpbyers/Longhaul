105c398f22314820f79470166a0df5bf70d184ff
anhonestmouse <anhonestmouse@gmail.com>
2018-02-01 17:21:43 -0700

    fixes staticfileconverter.  Now parses ALL THE DATA

--- back-end/controllers/mainController.js
+++ back-end/controllers/mainController.js
@@ -6,6 +6,8 @@ const DB = require('../models').models;
 const rtdUser = process.env.rtdUser || require('../config/env.js').rtdUser;
 const rtdPassword = process.env.rtdPassword || require('../config/env.js').rtdPassword;
 
+let updates = [];
+
 let getApi = (req, res)=>{
 	res.send('You got to the Get API request!');
 };
@@ -23,6 +25,35 @@ let getStops = (req, res)=>{
 
 //updates real-time trip data provided by RTD
 let updateData = ()=>{
+	let requestSettings = {
+	  method: 'GET',
+	  url: 'http://www.rtd-denver.com/google_sync/TripUpdate.pb',
+	  auth:{
+		user: 'rtdUser',
+		pass: 'rtdPassword',
+		sendImmediately: false
+		},
+	  encoding: null
+	};
+	request(requestSettings, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    let feed = GTFS.FeedMessage.decode(body);
+	    feed.entity.forEach(function(entity) {
+	      if (entity.trip_update) {
+	      	updates.push(entity.trip_update);
+	      	}
+	    });
+	  }
+	  mongo.connect(dburl, function(err, db){
+	  	db.db('RTDData').collection('update').remove({});
+	  	if(err) console.log('there has been an error');
+	  	updates.forEach((update)=>{
+	  		db.db('RTDData').collection('update').insert(update, (err,res)=>{
+	  			if(err) console.log('THERE HAS BEEN AN ERROR');
+	  		});
+	  	});
+	  });
+	});
 
 };
 
