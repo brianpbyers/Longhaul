543868c51eebc1e4ad9d905831873ff92782251a
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-02-01 18:00:53 -0700

    subscribes routesPage to getRoutes function

--- front-end/src/providers/bus-service/bus-service.ts
+++ front-end/src/providers/bus-service/bus-service.ts
@@ -5,18 +5,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class BusServiceProvider {
 
-  route: string;
-  stop: string;
-  bus: string;
+  busRoutes: any;
+  busStops: string;
+  busNumber: string;
 
   constructor(
     private http: HttpClient
   ) {}
 
   getRoutes(){
-    this.http.get('http://localhost:3000/api/routes').subscribe((routes) => {
-        console.log(routes);
-    })
+    return this.http.get('http://localhost:3000/api/routes')
   }
 
 
