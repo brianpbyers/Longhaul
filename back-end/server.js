9abf54528723d06f04748156ecf85ff7ee361ee0
chadkeeven <chadkeeven@gmail.com>
2018-02-05 10:24:25 -0700

    console logging port on heroku

--- back-end/server.js
+++ back-end/server.js
@@ -13,6 +13,8 @@ const session = require('express-session');
 //helps route requests
 const router = require('./config/routes');
 
+require('dotenv').config();
+
 
 
 
@@ -60,4 +62,4 @@ app.use('/', router);
 
 
 
-app.listen(process.env.PORT || 3000, console.log("Potato Server Listening on Port:", process.env.PORT ||3000));
+app.listen(process.env.PORT || 3000, console.log("Potato Server Listening on Port:", process.env ||3000));
