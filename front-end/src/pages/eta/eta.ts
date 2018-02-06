7d0514acd40e15fd91215c3abe257e10b0c6c62e
jasonbcox <jasonbcox@gmail.com>
2018-02-06 11:20:04 -0700

    local notifications installed but not tested

--- front-end/src/pages/eta/eta.ts
+++ front-end/src/pages/eta/eta.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, Platform, NavParams, AlertController } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import * as moment from 'moment';
 
 import { BusServiceProvider } from '../../providers/bus-service/bus-service';
 
@@ -17,12 +19,33 @@ export class EtaPage {
   busStop: any;
   bus: any;
   ETA: any;
+  notifyTime: any;
+  notifications: any[] = [];
+  days: any[];
+  chosenHours: number;
+  chosenMinutes: number;
 
   constructor(
     private navCtrl: NavController, 
+    public platform: Platform,
     private navParams: NavParams,
-    private busService: BusServiceProvider
-  ) {}
+    private busService: BusServiceProvider,
+    public alertCtrl: AlertController, 
+    public localNotifications: LocalNotifications
+  ) {this.notifyTime = moment(new Date()).format();
+    
+           this.chosenHours = new Date().getHours();
+           this.chosenMinutes = new Date().getMinutes();
+    
+           this.days = [
+               {title: 'Monday', dayCode: 1, checked: false},
+               {title: 'Tuesday', dayCode: 2, checked: false},
+               {title: 'Wednesday', dayCode: 3, checked: false},
+               {title: 'Thursday', dayCode: 4, checked: false},
+               {title: 'Friday', dayCode: 5, checked: false},
+               {title: 'Saturday', dayCode: 6, checked: false},
+               {title: 'Sunday', dayCode: 0, checked: false}
+           ];}
 
   ionViewDidLoad(){
 
@@ -36,6 +59,82 @@ export class EtaPage {
 
     this.updateEta();
   ;}
+
+timeChange(time){
+  this.chosenHours = time.hour.value;
+  this.chosenMinutes = time.minute.value;
+}
+
+addNotifications(){
+  
+  let currentDate = new Date();
+  let currentDay = currentDate.getDay(); // Sunday = 0, Monday = 1, etc.
+
+  for(let day of this.days){
+
+      if(day.checked){
+
+          let firstNotificationTime = new Date();
+          let dayDifference = day.dayCode - currentDay;
+
+          if(dayDifference < 0){
+              dayDifference = dayDifference + 7; // for cases where the day is in the following week
+          }
+
+          firstNotificationTime.setHours(firstNotificationTime.getHours() + (24 * (dayDifference)));
+          firstNotificationTime.setHours(this.chosenHours);
+          firstNotificationTime.setMinutes(this.chosenMinutes);
+
+          let notification = {
+              id: day.dayCode,
+              title: 'Hey!',
+              text: 'You just got notified :)',
+              at: firstNotificationTime,
+              every: 'week'
+          };
+
+          this.notifications.push(notification);
+
+      }
+
+  }
+
+  console.log("Notifications to be scheduled: ", this.notifications);
+
+  if(this.platform.is('cordova')){
+
+      // Cancel any existing notifications
+      this.localNotifications.cancelAll().then(() => {
+
+          // Schedule the new notifications
+          this.localNotifications.schedule(this.notifications);
+
+          this.notifications = [];
+
+          let alert = this.alertCtrl.create({
+              title: 'Notifications set',
+              buttons: ['Ok']
+          });
+
+          alert.present();
+
+      });
+
+  }
+ 
+}
+
+        
+cancelAll(){
+  this.localNotifications.cancelAll();
+  
+     let alert = this.alertCtrl.create({
+         title: 'Notifications cancelled',
+         buttons: ['Ok']
+     });
+  
+     alert.present();
+}
 
   updateEta(){
     setInterval(() => {
