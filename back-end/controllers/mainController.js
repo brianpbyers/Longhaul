f5c92cd8ea9f9489dce4f072a5874999bbb10aea
anhonestmouse <anhonestmouse@gmail.com>
2018-02-06 16:26:13 -0700

    fixes small issues

--- back-end/controllers/mainController.js
+++ back-end/controllers/mainController.js
@@ -130,7 +130,7 @@ let updateData = ()=>{
 //returns all saved routes associated with the user
 let getUserRoutes = (req, res)=>{
 	let user = Auth.decodeToken(req);
-	DaBa.sequelize.query(`SELECT user_routes.route_name, user_routes.stop_number, stops.name, routes.description FROM user_routes JOIN stops on user_routes.stop_number=stops.number JOIN routes on user_routes.route_name=routes.name WHERE user_routes."userId"=${user.id}`)
+	DaBa.sequelize.query(`SELECT user_routes.route_name, user_routes.stop_number, stops.name, routes.description FROM user_routes JOIN stops ON user_routes.stop_number=stops.number JOIN routes ON user_routes.route_name=routes.name WHERE user_routes."userId"=${user.id}`)
 	.then((routes)=>{
 		res.json(routes);
 	});
