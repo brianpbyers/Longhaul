a5ca7a745a22ca3c60ce418b5c9814c31326e8f1
anhonestmouse <anhonestmouse@gmail.com>
2018-02-07 15:59:39 -0700

    fixes issue #75

--- back-end/models/route.js
+++ back-end/models/route.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, Sequelize){
-//route model.  contains information not provided by real time data feed.  
+//route model.  contains information not provided by real time data feed.  Belongs to one update, and belongs to one user_route
   var model = sequelize.define('route', {
     name: Sequelize.STRING,
     description: Sequelize.STRING,
